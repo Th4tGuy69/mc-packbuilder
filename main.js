589e50de3ee02c5c73a55f45b8c0a2b5783f5fca
That Guy <thatguytf3@outlook.com>
2020-05-25 17:45:24 -0700

    organization

--- main.js
+++ main.js
@@ -1,7 +1,53 @@
+//#region Header
+
 //npm install -g electron
 //npm install -g mc-curseforge-api
 
 const { app, BrowserWindow } = require('electron')
+const cf = require('mc-curseforge-api')
+
+//#endregion
+
+//#region CurseForge
+
+class CurseForge {
+
+    GetMods(searchText, version, index, pages, callback)
+    {
+        cf.getMods({
+            searchFilter: searchText,
+            gameVersion: version,
+            index: index,
+            pageSize: pages,
+            callback: cb
+        }).then((mods) => {
+            console.log(mods)
+        })
+    }
+
+    GetMod(callback)
+    {
+        cf.getMod(callback).then((mod) => { console.log(mod) })
+    }
+
+    GetModFiles(id, callback)
+    {
+        cf.getModFiles(id, callback).then((files) => {
+            files[0].download("./TestDownloads/Mod.jar")
+        })
+    }
+
+    GetModDescription(id, callback)
+    {
+        cf.GetModDescription(id, callback).then((desc) => {
+            console.log(desc)
+        })
+    }
+}
+
+//#endregion
+
+//#region Electron
 
 function createWindow() 
 {
@@ -22,3 +68,5 @@ function createWindow()
 }
 
 app.on('ready', createWindow)
+
+//#endregion
